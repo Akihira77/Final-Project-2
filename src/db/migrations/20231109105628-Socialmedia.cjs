9155658f78d6010cb444010ab27049d37ea3ea74
Fajrian11 <nugraha.fajrian@gmail.com>
2023-11-12 10:12:12 +0700

    update migration socialmedias

--- src/db/migrations/20231109105628-Socialmedia.cjs
+++ src/db/migrations/20231109105628-Socialmedia.cjs
@@ -1,22 +1,58 @@
-'use strict';
-
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add altering commands here.
+		 *
+		 * Example:
+		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		 */
+		await queryInterface.createTable("Socialmedias", {
+			id: {
+				primaryKey: true,
+				allowNull: false,
+				autoIncrement: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				allowNull: false,
+				type: Sequelize.STRING,
+			},
+			social_media_url: {
+				allowNull: false,
+				type: Sequelize.STRING,
+			},
+			UserId: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				references: {
+					model: "Users",
+					key: "id",
+				},
+				onDelete: "CASCADE",
+				onUpdate: "CASCADE",
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Date.now(),
+			},
+		});
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+		await queryInterface.dropTable("Socialmedias");
+	},
 };
+
